2f1c24eb10a4dcb0165b5c2e306eb994a85556bc
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2021-11-11 17:18:55 +0000

    Less coverage

--- other/src/main/java/org/creek/other/Library.java
+++ other/src/main/java/org/creek/other/Library.java
@@ -6,9 +6,7 @@ public class Library {
     }
 
     public boolean someOtherLibraryMethod() {
-
         final String willNotFindMe = System.getenv("WillNotFindMe");
-        final String willNotFindMeEither = System.getenv("WillNotFindMeEither");
         System.out.println("Just adding some more lines");
         if (willNotFindMe != null) {
             return true;
