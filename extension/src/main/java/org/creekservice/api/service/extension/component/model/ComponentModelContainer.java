b345518e90182dcaf6e167cdf942de60c77a7699
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-09-15 18:31:39 +0100

    Avoid users having to cast resource handlers to raw types

--- extension/src/main/java/org/creekservice/api/service/extension/component/model/ComponentModelContainer.java
+++ extension/src/main/java/org/creekservice/api/service/extension/component/model/ComponentModelContainer.java
@@ -17,6 +17,8 @@
 package org.creekservice.api.service.extension.component.model;
 
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import org.creekservice.api.platform.metadata.ResourceDescriptor;
 import org.creekservice.api.platform.metadata.ResourceHandler;
 
@@ -30,11 +32,67 @@ public interface ComponentModelContainer extends ComponentModelCollection {
      * extension needs to handle.
      *
      * <p>Resources can only be registered during the call to {@link
-     * org.creekservice.api.service.extension.CreekExtensionProvider#initialize(org.creekservice.api.service.extension.CreekService)}
+     * org.creekservice.api.service.extension.CreekExtensionProvider#initialize}
      *
      * @param type the custom recourse type.
      * @return self, to allow for method chaining.
      */
     <T extends ResourceDescriptor> ComponentModelContainer addResource(
             Class<T> type, ResourceHandler<? super T> handler);
+
+    /**
+     * Register a custom resource type with Creek.
+     *
+     * <p>This is a convenience method to avoid having to cast resource and handlers that use
+     * generics to raw types. The underlying registration is still by the raw class. It is therefore
+     * not possible to register handlers for type refs that share the same raw class.
+     *
+     * <p>It is recommended that all type parameters of the {@code type} parameter as wildcards. For
+     * example:
+     *
+     * <pre>{@code
+     * interface MyResource<T> {
+     *     ...
+     * }
+     *
+     * class MyResourceHandler ResourceHandler<MyResource<?>> {
+     *     ...
+     * }
+     *
+     * api.components()
+     *     .model()
+     *     .addResource(new HandlerTypeRef<MyResourceHandler<?>>() {}, new MyResourceHandler());
+     * }</pre>
+     *
+     * @param type the custom recourse type, which can contain generics.
+     * @return self, to allow for method chaining.
+     * @see ComponentModelContainer#addResource(Class, ResourceHandler)
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    default <T extends ResourceDescriptor> ComponentModelContainer addResource(
+            HandlerTypeRef<T> type, ResourceHandler<? super T> handler) {
+        return addResource((Class) type.type, (ResourceHandler) handler);
+    }
+
+    /**
+     * A help type used by {@link #addResource(HandlerTypeRef, ResourceHandler)}.
+     *
+     * @see ComponentModelContainer#addResource(HandlerTypeRef, ResourceHandler)
+     */
+    @SuppressWarnings("unused")
+    class HandlerTypeRef<T extends ResourceDescriptor> {
+        private final Class<?> type;
+
+        protected HandlerTypeRef() {
+            final Type superClass = this.getClass().getGenericSuperclass();
+            if (!(superClass instanceof ParameterizedType)) {
+                throw new IllegalArgumentException("HandlerTypeRef constructed as raw type");
+            }
+            Type type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
+            if (type instanceof ParameterizedType) {
+                type = ((ParameterizedType) type).getRawType();
+            }
+            this.type = (Class<?>) type;
+        }
+    }
 }
