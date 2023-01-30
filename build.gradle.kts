34005423ab3abe8ef09287453532fd48cd2fcc45
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2023-01-30 23:44:00 +0000

    Update build.gradle.kts (#138)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ subprojects {
     }
 
     extra.apply {
-        set("creekVersion", "0.3.1")
+        set("creekVersion", "0.3.2-SNAPSHOT")
         set("spotBugsVersion", "4.7.3")         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
 
         set("log4jVersion", "2.19.0")           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
